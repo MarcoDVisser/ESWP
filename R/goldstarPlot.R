28c37d0f54b784f3fc1e4f6bc9a901df4e131f6b
Marco Visser <marco.d.visser@gmail.com>
2019-04-10 10:15:50 -0400

    Weekly update

--- R/goldstarPlot.R
+++ R/goldstarPlot.R
@@ -33,33 +33,70 @@ stkDates <- stkDates[stkDates>d]
 stkWeeks <- as.numeric(format(stkDates,format="%V"))
 
 hits <- star <- numeric(52)
-hits[weeks%in%stkWeeks] <- stk$words>300
-star[weeks%in%stkWeeks][stk$words>300] <- "goldenrod4"
-star[weeks%in%stkWeeks][stk$words>1000] <- "grey87"
-star[weeks%in%stkWeeks][stk$words>1000] <- "gold2"
 
- 
-png("./streakFig.png",width=800,height=600)
+for(i in stkWeeks){
+    tmp <- sum(stk$words[stkWeeks==i])
+
+    if(tmp>300){
+        hits[weeks==i] <- TRUE
+        star[weeks==i] <- "goldenrod4"
+        
+    if(tmp>1000){
+        star[weeks==i]<- "grey87"
+    }
+        
+    if(tmp>2000){
+        star[weeks==i] <- "gold2"
+    }
+    }
+}
+
+   
+png("./streakFig.png",width=900,height=600)
 
 layout(matrix(1:60,nrow=6,ncol=10,byrow=TRUE))
 
 
-par(mar=c(0,0,1,0))
+par(mar=c(0,0,1,0),xpd=FALSE)
 for(i in 1:60){
 
     if(i%in%wks){
 
+        
         plot(0,0,ylim=c(-1,1),xlim=c(-1,1),fg="white",xaxt="n",yaxt="n",
-             xlab="",ylab="",main=weeks[i],col="white")
+             xlab="",ylab="",main=paste0("Week ",weeks[i]),col="white")
 
+        rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],
+             col=ifelse(weeks[i]==thisweek,"lightcyan","white"))
+        
         if(hits[i]){
 
-            symbols(0,0,fg="gold",bg=star[i]
+            symbols(0,0,fg=ifelse(star[i]=="gold2","grey60","gold4"),bg=star[i]
                    ,stars=rbind(rep(c(2,1,1),5)),add=TRUE,
-                    inches=.25)
-        }
-    }
+                    inches=.35)
+        } else {
 
+
+            points(0,0,col="red",pch=4,cex=5)
+
+        }
+    } else {
+
+             plot(0,0,ylim=c(-1,1),xlim=c(-1,1),fg="white",xaxt="n",yaxt="n",
+                  xlab="",ylab="",main="",col="white")
+
+
+    
+    if(i==55) {
+
+        par(xpd=NA)
+
+        legend("top",col=c("goldenrod4","grey87","gold2"),
+                    pch=16,legend=c("> 300 words","> 1000 words","> 2000 words")
+                   ,bty="n",cex=2.5) }
+
+    }
 }
+        par(xpd=TRUE)
 
 dev.off()
